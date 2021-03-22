a6fcfec38dd34318ddccfd4ad8068a5720f3e342
calumpetergunn <calumgunn@gmail.com>
2021-03-22 10:40:10 +0000

    basic test files populated

--- tests/product_test.py
+++ tests/product_test.py
@@ -6,5 +6,31 @@ class TestProduct(unittest.TestCase):
     def setUp(self):
         self.product = Product("Castlemania", "Thee Oh Sees", "Castleface Records", "LP", "Psychedelic", 25, 9.50, 15.99)
 
-    def test_product_has_name(self):
-        self.assertEqual("Castelmania", self.product.name)
+    def test_product_has_title(self):
+        self.assertEqual("Castlemania", self.product.title)
+
+    def test_product_has_artist(self):
+        self.assertEqual("Thee Oh Sees", self.product.artist)
+
+    def test_product_has_record_label(self):
+        self.assertEqual("Castleface Records", self.product.record_label)
+
+    def test_product_has_format(self):
+        self.assertEqual("LP", self.product.format)
+        
+    def test_product_has_genre(self):
+        self.assertEqual("Psychedelic", self.product.genre)
+        
+    def test_product_has_quantity(self):
+        self.assertEqual(25, self.product.quantity)
+        
+    def test_product_has_buy_cost(self):
+        self.assertEqual(9.50, self.product.buy_cost)
+        
+    def test_product_has_sell_price(self):
+        self.assertEqual(15.99, self.product.sell_price)
+
+
+
+
+
--- tests/record_label_test.py
+++ tests/record_label_test.py
@@ -4,7 +4,10 @@ from models.record_label import RecordLabel
 class TestRecordLabel(unittest.TestCase):
 
     def setUp(self):
-        self.record_label = RecordLabel("Castleface Records", "San Fransisco")
+        self.record_label = RecordLabel("Castleface Records", "San Francisco")
 
     def test_record_label_has_name(self):
         self.assertEqual("Castleface Records", self.record_label.name)
+
+    def test_record_label_has_location(self):
+        self.assertEqual("San Francisco", self.record_label.location)
