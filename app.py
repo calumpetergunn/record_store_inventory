eb9c5beb5e8b22bd344b3d3ab715b23b24b34d3b
calumpetergunn <calumgunn@gmail.com>
2021-03-22 13:42:39 +0000

    controllers both linking to pages

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 from controllers.product_controller import products_blueprint
+from controllers.record_label_controller import record_labels_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(products_blueprint)
+app.register_blueprint(record_labels_blueprint)
 
 @app.route('/')
 def home():
--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -4,3 +4,8 @@ from repositories import product_repository, record_label_repository
 from models.product import Product
 
 products_blueprint = Blueprint("products", __name__)
+
+@products_blueprint.route("/products")
+def products():
+    products = product_repository.select_all()
+    return render_template("products/index.html", products = products)
